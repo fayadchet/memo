9b79030d6c3be5352c049b6a17eba391d1dc23df
fayadchet <chettaouifayad@gmail.com>
2017-02-20 17:39:05 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,24 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+
+//Importer le module des routes
+import { Routing } from './routing';
+import { MemoComponent } from './components/memo/memo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    MemoComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    //Ajouter la class des routes
+    Routing
   ],
   providers: [],
   bootstrap: [AppComponent]
